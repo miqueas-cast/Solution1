78bef9bb0d3522fe490ed5f293fe2e3b80815d82
MiqueasCast <castmicah@gmail.com>
2026-04-27 19:50:46 -0600

    Nuevos ejercicios

---
+++ Ejercicio8/Program.cs
@@ -0,0 +1,23 @@
+﻿static int ValidacionEntradas(string mensaje, int min, int max)
+{
+    int valor;
+    bool esValido;
+    do
+    {
+        Console.Write(mensaje);
+        string entrada = Console.ReadLine();
+        esValido = int.TryParse(entrada, out valor) && valor >= min && valor <= max;
+        if (!esValido)
+        {
+            Console.Clear();
+            Console.WriteLine($"Por favor, ingresa un número.");
+        }
+        if (valor < min && valor > max)
+        {
+            Console.WriteLine("Valor fuera del rango");
+        }
+    } while (!esValido);
+    return valor;
+}
+
+int jugadores = 
